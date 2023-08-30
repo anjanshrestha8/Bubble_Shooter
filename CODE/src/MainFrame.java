34b0ac827a399c483ae5d06f0c10d41470032bed
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-08-30 20:28:47 +0545

    main and mainFrame code completed

--- CODE/src/MainFrame.java
+++ CODE/src/MainFrame.java
@@ -1,2 +1,77 @@
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
+
+
+/**
+ * this class produces the window and gives a frame to the
+ * whole application
+ *
+ */
+public class MainFrame extends JFrame implements ActionListener {
+    private SettingPanel rightPanel;
+    private Canvas leftPanel;
+
+    /**
+     * constructor, initiates the components, and sets the
+     * properties of the frame
+     */
+    public MainFrame(){
+        setLayout(new BorderLayout());
+
+        rightPanel = new SettingsPanel(this);
+        rightPanel.initComponents();
+
+        leftPanel = new Canvas();
+
+        add(leftPanel, BorderLayout.WEST);
+        add(rightPanel, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Bubble Shooter");
+        setSize(Constants.WINDOW_SIZE_X, Constants.WINDOW_SIZE_Y);
+        setResizable(true);
+        setVisible(true);
+        setSize(2*Constants.WINDOW_SIZE_X-getContentPane().getSize().width,
+                2*Constants.WINDOW_SIZE_Y-getContentPane().getSize().height);
+
+    }
+
+    /**
+     * makes the left panel display the highscores
+     */
+    public void init(){
+        leftPanel.displayHighscore(0,true);
+    }
+
+/**
+ * can be called when the game is won, displays the highscores
+ *  */
+
+    public void gameWon(long score){
+        rightPanel.updateScore(score);
+        leftPanel.dispalayHightScore(score,false);
+
+    }
+
+
+    public void updateScore(long score){
+        rightPanel.updateScore(score);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("NEW GAME"));{
+            leftPanel.newGame(rightPanel.getRow(),rightPanel.getColor());
+            leftPanel.getGame().setMainFrame(this);
+        }
+         else if (e.getActionCommand().equals("STOP GAME")){
+            if(leftPanel.getGame()!=null){
+                leftPanel.getGame().stop();
+                leftPanel.displayHighScore(0,true);
+            }
+         }
+    }
 }
--- CODE/src/SettingPanel.java
+++ CODE/src/SettingPanel.java
@@ -1,2 +1,45 @@
-public class SettingPanel {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+
+public class SettingPanel extends JPanel {
+//    private MainFrame mainFrame;
+    private JLabel scoreLabel;
+    private JPanel lowerPanel;
+    private JSpinner rowSpinner;
+    private JSpinner colorSpinner;
+    private JButton newGameButton;
+    private JButton stopGameButton;
+
+    public SettingPanel()
+    {
+        setBorder(BorderFactory.createMatteBorder(0,1,0,0, Color.BLACK));
+        setLayout(new BorderLayout());
+//        mainFrame=m;
+    }
+    public void initComponents(){
+        scoreLabel=new JLabel("0",SwingConstants.RIGHT);
+        scoreLabel.setBorder(BorderFactory.createMatteBorder(0,0,1,0,Color.blue));
+        scoreLabel.setPreferredSize(new Dimension(Constants.WINDOW_SIZE_X-Constants.FIELD_SIZE_X-5,50));
+        scoreLabel.setFont(new Font("Courier",Font.BOLD,34));
+
+
+        lowerPanel = new JPanel();
+        lowerPanel.setLayout(new BoxLayout(lowerPanel,BoxLayout.Y_AXIS));
+
+        JPanel rowsPanel = new JPanel();
+        rowsPanel.setPreferredSize(new Dimension(Constants.Window));
+    }
+
 }
